7146dd3ec42be2c7ae19bad8520f473ae2c7bc4e
Ksnat <Ksnat@KASINATHAN.lan>
2024-02-01 17:25:21 -0800

    new class added

---
+++ LearnViaMaven/src/main/java/SeleniumTest/TestGitHubMergee.java
@@ -0,0 +1,14 @@
+package SeleniumTest;
+
+public class TestGitHubMergee {
+
+	public TestGitHubMergee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
