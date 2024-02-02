a82fb909ac034a01ef31617b798aa9ac648ec7d8
Ksnat <Ksnat@KASINATHAN.lan>
2024-02-01 17:31:24 -0800

    added second class

---
+++ LearnViaMaven/src/main/java/SeleniumTest/TestClasssTwo.java
@@ -0,0 +1,16 @@
+package SeleniumTest;
+
+public class TestClasssTwo {
+
+	public TestClasssTwo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("this is creatd to test the Merge in GitHub");
+	}
+
+}
